e51ab19a5095775f7bebafe218154afabd405c68
Alejandro <ale.lazarte@hotmail.com>
2017-06-01 18:20:25 -0300

    Add avg function

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Simple CSV Parser in PHP
 
+This repo contains a simple script written in PHP, that parses a csv file in 
+a specific format, to another csv.
+
+The script takes as an argument the name of the csv file that you want to 
+parse. The csv has to have the following format:
+
+```
+\[device\]\[objetct\]\[indicator\]\[timestamp\]\[value\]
+```
+
+The output csv reads each line and generates files depending on the timestamp 
+found in the input file. The files generated contains the summation of all 
+the values, and its average, grouped by device and object.
+
+The name of the output files should be identified by timestamp:
+
+```
+results\_[timestamp].csv
+```
+
+The output files should have the following format:
+
+```
+\[device\]\[objetct\]\[sum\]\[timestamp\]\[sum all values\]
+```
+```
+\[device\]\[objetct\]\[count\]\[timestamp\]\[avg all values\]
+```
+
+The idea to store the result was having a multidimensional array, to 
+distinguish timestamps. Then, each timestamp have its own set of arrays to 
+store its average and summations, where the key in each one is the device and 
+the object.
+
+### Reference sites
+
+#### Functions from PHP manual
+
+[fgetcsv()](http://php.net/manual/en/function.fgetcsv.php)
+[foreach()](http://php.net/manual/en/control-structures.foreach.php)
+[explore()](http://php.net/manual/en/function.explode.php)
+[$argv](http://php.net/manual/en/reserved.variables.argv.php)
+[class and visibility](http://php.net/manual/en/language.oop5.late-static-bindings.php)
+
+#### Similar repositories
+
+[PHPCsvParser](https://github.com/kzykhys/PHPCsvParser)
+
+#### Stackoverflow questions
+
+[Search in multidimensional array](https://stackoverflow.com/questions/8102221/php-multidimensional-array-searching-find-key-by-specific-value)
+[Delete key from array](https://stackoverflow.com/questions/5450148/php-remove-key-from-associative-array)
--- src/class.php
+++ src/class.php
@@ -1,7 +1,7 @@
 <?php
 class ParseCsv
 {
-    private $timestamp;
+    private $timestamp, $header;
 
     function __construct()
     {
@@ -29,11 +29,23 @@ class ParseCsv
             $this->timestamp[$data[3]]["count"][$data[0].",".$data[1]] = 1;
     }
 
+    private function averages()
+    {
+        foreach($this->timestamp as $tms => $tuple)
+        {
+            foreach($tuple["count"] as $dev_obj => $count)
+            {
+                $this->timestamp[$tms]["avg"][$dev_obj] = $this->timestamp[$tms]["sums"][$dev_obj] / $count;
+            }
+        }
+        unset($this->timestamp["count"]);
+    }
+
     public function readCsv($file)
     {
         if (($handle = fopen($file,"r")) !== FALSE)
         {
-            $header = ($data = fgetcsv($handle,50,",") !== FALSE) ? $data : array(null);
+            $this->header = ($data = fgetcsv($handle,50,",") !== FALSE) ? $data : array(null);
             while(($data = fgetcsv($handle,50,",")) !== FALSE)
             {
                 if(array(null) !== $data)
@@ -45,34 +57,37 @@ class ParseCsv
         }
     }
 
+    private function cleanFiles()
+    {
+        foreach(array_keys($this->timestamp) as $key)
+        {
+            file_put_contents("../data/results_$key.csv","");
+        }
+    }
+
+    private function writeHeaders()
+    {
+        foreach(array_keys($this->timestamp) as $key)
+        {
+            file_put_contents("../data/results_$key.csv","");
+        }
+    }
+
     public function write()
     {
-        foreach($this->timestamp as $key1 => $subarray)
+        $this->cleanFiles();
+        $this->writeHeader();
+        foreach($this->timestamp as $tms => $data)
         {
-            foreach($subarray as $key2 => $operation)
+            foreach($data as $op_type => $tuple)
             {
-                foreach($operation as $key3 => $val)
+                foreach($tuple as $dev_obj => $val)
                 {
-                    file_put_contents("results_$key1.csv", "$key3,$key2,$key1,$val\n",FILE_APPEND);
+                    file_put_contents("../data/results_$tms.csv", "$dev_obj,$op_type,$tms,$val\n",FILE_APPEND);
                 }
             }
         }
     }
-
-    public function show()
-    {
-        foreach($this->timestamp as $key1 => $subarray)
-        {
-            foreach($subarray as $key2 => $operation)
-            {
-                foreach($operation as $key3 => $val)
-                {
-                    echo "$key3,$key2,$key1,$val\n";
-                }
-            }
-        }
-    }
-
 }
 
 $p = new ParseCsv();
